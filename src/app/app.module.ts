a5c0a925def8b1f9090d689b4e686a62939e19f6
star0914 <58557458+star0914@users.noreply.github.com>
2020-05-17 16:58:56 +0300

    login, home, and tab pages design and integration face login using fingerprint npm plugin.
    TODO : fingerprint plugin have a issue.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,9 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {LoginPage} from './login/login.page';
-import {FingerprintAIO} from '@ionic-native/fingerprint-aio';
 import {LoginPageModule} from './login/login.module';
 import {ComponentModule} from './widgets/component.module';
+// import {FingerprintAIO} from '@ionic-native/fingerprint-aio'; //TODO
 
 @NgModule({
   declarations: [AppComponent],
@@ -27,7 +27,7 @@ import {ComponentModule} from './widgets/component.module';
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    FingerprintAIO
+    // FingerprintAIO TODO
   ],
   bootstrap: [AppComponent]
 })
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,7 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {ModalController} from '@ionic/angular';
-import {FingerprintAIO} from '@ionic-native/fingerprint-aio';
+// import {FingerprintAIO} from '@ionic-native/fingerprint-aio'; // TODO
 
 @Component({
     selector: 'app-login',
@@ -13,7 +13,7 @@ export class LoginPage implements OnInit {
 
     constructor(
         private router: Router,
-        private faio: FingerprintAIO,
+        // private faio: FingerprintAIO, // TODO
         private modalCtrl: ModalController
     ) {
     }
@@ -23,19 +23,21 @@ export class LoginPage implements OnInit {
     }
 
     login() {
-        this.faio.show({
-            clientId: '43580117595-t4ca4g8lidha4a15942dd4uja60csi81.apps.googleusercontent.com',
-            clientSecret: 'MJeNE2PJjVppYP9kaFVE74Nm',
-            disableBackup: true,  // Only for Android(optional)
-            // localizedFallbackTitle: 'Use Pin', // Only for iOS
-            // localizedReason: 'Please authenticate' // Only for iOS
-        }).then(() => {
+
+        // TODO fingerprint plugin used
+        // this.faio.show({
+        //     clientId: '43580117595-t4ca4g8lidha4a15942dd4uja60csi81.apps.googleusercontent.com',
+        //     clientSecret: 'MJeNE2PJjVppYP9kaFVE74Nm',
+        //     disableBackup: true,  // Only for Android(optional)
+        //     // localizedFallbackTitle: 'Use Pin', // Only for iOS
+        //     // localizedReason: 'Please authenticate' // Only for iOS
+        // }).then(() => {
             if (this.isModal) {
                 this.modalCtrl.dismiss();
             } else {
                 this.router.navigateByUrl('/tabs/home');
             }
-        })
+        // })
     }
 
 }
